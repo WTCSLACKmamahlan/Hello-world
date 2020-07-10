c6c5d3f207e0213e6a20a97ebba2455d7498ab97
WTCSLACKmamahlan <67944799+WTCSLACKmamahlan@users.noreply.github.com>
2020-07-10 13:00:04 +0200

    Add files via upload

---
+++ ex00/ft_print_alphabet.c
@@ -0,0 +1,19 @@
+#include <studio,h>
+
+void     ft_print_alphabet(void)
+{
+	char     lette;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		putchar(letter)
+			letter++;
+	}
+}
+
+int main(void)
+{
+	ft_print_alphabet();
+	return 0;
+}
