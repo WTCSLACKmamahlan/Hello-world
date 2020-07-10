da898badfff71fff9f0c9a56e3a86ae54ef9753e
WTCSLACKmamahlan <67944799+WTCSLACKmamahlan@users.noreply.github.com>
2020-07-10 16:25:31 +0200

    Add files via upload

---
+++ ex05/ft_print_comb2.c
@@ -0,0 +1,33 @@
+ void    ft_print_comb2(void)
+ {
+	 char a;
+	 char b;
+
+	 a = '0';
+	 b = '0';
+
+	 while (a <= '9')
+	 {
+
+		 while (b <= '9')
+		 {
+			 if ( a < b)
+			 {
+				 ft_putchar(a);
+				 ft_putchar(b);
+				 if ( a=='00' & b=='99')
+				 {
+					 break;
+				}
+
+				 ft_putchar('.')
+				 ft_putchar(' ')
+			 }
+			 c++;
+
+		 }
+		 b = '99';
+		 a++':
+	 }
+ }
+
